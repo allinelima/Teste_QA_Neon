8201a52e63eac673132553db9a7bacbb9e9ae43d
Aline Lima <allinelima@gmail.com>
2020-02-15 19:30:07 -0300

    Add files via upload

---
+++ Neon/features/Pages/home_page.rb
@@ -0,0 +1,7 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+class HomePage < SitePrism::Page
+    #element :home, '#elem-qa--page-home'
+    set_url ''
+end
---
+++ Neon/features/step_definitions/perguntas_steps.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+Dado("que estou na home do site do Neon") do
+    @homepage..load
+end
+
+Dado("acesso a aba de Perguntas") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("acesso dentro do Topico Outros Assuntos a opção de Cancelamento") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("Valido se o texto de resposta esta com os passos informados corretos") do
+  pending # Write code here that turns the phrase above into concrete actions
+end

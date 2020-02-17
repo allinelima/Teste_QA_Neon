e5653e6f17b66dc733c5296b8ca32037f15b2033
Aline Lima <allinelima@gmail.com>
2020-02-16 21:40:24 -0300

    Update perguntas_steps.rb

--- features/step_definitions/perguntas_steps.rb
+++ features/step_definitions/perguntas_steps.rb
@@ -7,9 +7,6 @@ end
 
 Dado("acesso a aba de Perguntas") do
   @homepage.acessar_perguntas
-  #expect(find('ul[class="header__nav-list""]')).to have_text 'Perguntas'
-  #expect(find('h3[class="questions__category-title"]')).to have_text 'Outros assuntos'
-  #expect(find('li[class="questions__section--visible"]')).to have_text 'Cancelamento'
 end
 
 Quando("acesso dentro do Topico Outros Assuntos a opção de Cancelamento") do
@@ -25,9 +22,9 @@ end
 
 Quando("acesso a Abra sua conta dentro da aba Pejota") do
   @homepage.acessar_pejota
-  #expect(find('li[ class="header__nav-list-item"]')).to have_text 'Pejota'
 end
 
-Então("Valido quais documentos são validos na opção") do
-  pending # Write code here that turns the phrase above into concrete actions
+Então("Então verifico quais documentos de todos os socios são necessários.") do
+  @homepage.acessar_lista
+  page.assert_selector('p', :text => 'São válidos os documentos RG, CNH ou RNE (no caso de estrangeiros).', :visible => true)
 end
